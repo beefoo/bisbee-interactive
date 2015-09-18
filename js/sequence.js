87a1a08c961fcc1e98258cabab4e604bea207bcb
Brian Foo <brian@youaremyjoy.org>
2015-09-17 21:29:26 -0400

    fix debug

--- js/sequence.js
+++ js/sequence.js
@@ -3,40 +3,64 @@ var BisbeeSequence = [
     start: '0:05',
     end: '0:07',
     onReset: function(b){
+      $('#debug-scene').text('--');
+      $('#debug-progress').text('--');
+    },
+    onStart: function(b){
+      console.log('Starting #1');
       $('#debug-scene').text('Scene 1');
       $('#debug-progress').text('0%');
     },
-    onStart: function(b){console.log('Starting #1');},
     onProgress: function(b){
       $('#debug-scene').text('Scene 1');
       $('#debug-progress').text(Math.round((b.currentTime-this.start)/(this.end-this.start)*100)+'%');
     },
-    onEnd: function(b){console.log('Ending #1');}
+    onEnd: function(b){
+      console.log('Ending #1');
+      $('#debug-scene').text('--');
+      $('#debug-progress').text('--');
+    }
   },{
     start: '0:07',
     end: '0:12',
     onReset: function(b){
-      $('#debug-scene').text('Scene 1');
+      $('#debug-scene').text('--');
+      $('#debug-progress').text('--');
+    },
+    onStart: function(b){
+      console.log('Starting #2');
+      $('#debug-scene').text('Scene 2');
       $('#debug-progress').text('0%');
     },
-    onStart: function(b){console.log('Starting #2');},
     onProgress: function(b){
       $('#debug-scene').text('Scene 2');
       $('#debug-progress').text(Math.round((b.currentTime-this.start)/(this.end-this.start)*100)+'%');
     },
-    onEnd: function(b){console.log('Ending #2');}
+    onEnd: function(b){
+      console.log('Ending #2');
+      $('#debug-scene').text('--');
+      $('#debug-progress').text('--');
+    }
   },{
     start: '0:15',
     end: '0:35',
     onReset: function(b){
-      $('#debug-scene').text('Scene 1');
+      $('#debug-scene').text('--');
+      $('#debug-progress').text('--');
+    },
+    onStart: function(b){
+      console.log('Starting #3');
+      $('#debug-scene').text('Scene 3');
       $('#debug-progress').text('0%');
     },
-    onStart: function(b){console.log('Starting #3');},
     onProgress: function(b){
       $('#debug-scene').text('Scene 3');
       $('#debug-progress').text(Math.round((b.currentTime-this.start)/(this.end-this.start)*100)+'%');
     },
-    onEnd: function(b){console.log('Ending #3');}
+    onEnd: function(b){
+      console.log('Ending #3');
+      $('#debug-scene').text('--');
+      $('#debug-progress').text('--');
+    }
   }
 ];
