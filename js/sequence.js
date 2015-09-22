dba3b5979fcb91c6728a7c89f2d1ad214f687633
beefoo <brian@youaremyjoy.org>
2015-09-22 18:19:31 -0400

    Adjust door sequence

--- js/sequence.js
+++ js/sequence.js
@@ -6,13 +6,31 @@ var BisbeeSequence = [
     animate: {
       left: [-100, 50, '%']
     },
-    off: function(b){},
-    onStart: function(b){
-      if (b.direction > 0) b.mediaPlay('door-in', true);
+    played_door_in: false,
+    played_door_out: false,
+    off: function(b){
+      this.played_door_in = false;
+      this.played_door_out = false;
     },
+    onStart: function(b){},
     onProgress: function(p,b){
       var t = BisbeeTween.inPauseOut(p, 0.1);
       BisbeeTween.tween(this.$el, this.animate, t);
+
+      // classes
+      if (p.between(0.3, 0.7)) this.$el.removeClass('moving-horizontal');
+      else this.$el.addClass('moving-horizontal');
+
+      // sounds
+      var sound_delay = 0.2;
+      if ((b.direction > 0 && p > sound_delay || b.direction < 0 && p < sound_delay) && !this.played_door_in) {
+        this.played_door_in = true;
+        b.mediaPlay('door-in', true);
+      }
+      if ((b.direction > 0 && p > (0.5+sound_delay) || b.direction < 0 && p < (0.5+sound_delay)) && !this.played_door_out) {
+        this.played_door_out = true;
+        b.mediaPlay('door-out', true);
+      }
     },
     onEnd: function(b){}
   },{
@@ -22,13 +40,31 @@ var BisbeeSequence = [
     animate: {
       right: [-100, 50, '%']
     },
-    off: function(b){},
-    onStart: function(b){
-      if (b.direction > 0) b.mediaPlay('door-in', true);
+    played_door_in: false,
+    played_door_out: false,
+    off: function(b){
+      this.played_door_in = false;
+      this.played_door_out = false;
     },
+    onStart: function(b){},
     onProgress: function(p,b){
       var t = BisbeeTween.inPauseOut(p, 0.1);
       BisbeeTween.tween(this.$el, this.animate, t);
+
+      // classes
+      if (p.between(0.3, 0.7)) this.$el.removeClass('moving-horizontal');
+      else this.$el.addClass('moving-horizontal');
+
+      // sounds
+      var sound_delay = 0.2;
+      if ((b.direction > 0 && p > sound_delay || b.direction < 0 && p < sound_delay) && !this.played_door_in) {
+        this.played_door_in = true;
+        b.mediaPlay('door-in', true);
+      }
+      if ((b.direction > 0 && p > (0.5+sound_delay) || b.direction < 0 && p < (0.5+sound_delay)) && !this.played_door_out) {
+        this.played_door_out = true;
+        b.mediaPlay('door-out', true);
+      }
     },
     onEnd: function(b){}
   },{
@@ -38,13 +74,41 @@ var BisbeeSequence = [
     animate: {
       top: [-100, 50, '%']
     },
-    off: function(b){},
-    onStart: function(b){
-      if (b.direction > 0) b.mediaPlay('door-down', true);
+    played_door_in: false,
+    played_door_out: false,
+    played_door_open: false,
+    off: function(b){
+      this.played_door_in = false;
+      this.played_door_out = false;
+      this.played_door_open = false;
     },
+    onStart: function(b){},
     onProgress: function(p,b){
       var t = BisbeeTween.inPauseOut(p, 0.1);
       BisbeeTween.tween(this.$el, this.animate, t);
+
+      // classes
+      if (p.between(0.3, 0.7)) this.$el.removeClass('moving-vertical');
+      else this.$el.addClass('moving-vertical');
+
+      var door_open_p = 0.5;
+      if (p > door_open_p) this.$el.addClass('open');
+      else this.$el.removeClass('open');
+
+      // sounds
+      var sound_delay = 0.2;
+      if ((b.direction > 0 && p > sound_delay || b.direction < 0 && p < sound_delay) && !this.played_door_in) {
+        this.played_door_in = true;
+        b.mediaPlay('door-in', true);
+      }
+      if ((b.direction > 0 && p > (0.5+sound_delay) || b.direction < 0 && p < (0.5+sound_delay)) && !this.played_door_out) {
+        this.played_door_out = true;
+        b.mediaPlay('door-out', true);
+      }
+      if (b.direction > 0 && p > door_open_p && !this.played_door_open) {
+        this.played_door_open = true;
+        b.mediaPlay('door-open', true);
+      }
     },
     onEnd: function(b){}
   }
